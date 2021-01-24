90191e5d00de11978aa8f938ba653d020a8ed311
wdlkmpx <wdlkmpx@gmail.com>
2021-01-24 18:20:10 +0800

    score.c: consistent indentation

--- src/score.c
+++ src/score.c
@@ -40,113 +40,123 @@
 
 static char * choose_score_file (void)
 { // returns a string that must be freed
-  const char * home_dir = g_getenv ("HOME");
-  gchar *score_path = g_strconcat (home_dir, G_DIR_SEPARATOR_S, ".gtktetcolor_score", NULL);
-  return (score_path);
+   const char * home_dir = g_getenv ("HOME");
+   gchar *score_path = g_strconcat (home_dir, G_DIR_SEPARATOR_S, ".gtktetcolor_score", NULL);
+   return (score_path);
 }
 
-gboolean
-read_score (void)
-{
-  gint i, j;
-  gint cksum;
-  struct stat file_info;
-  char * score_path = choose_score_file ();
-  FILE * score_file;
 
-  if ((score_file = fopen (score_path, "r")) == NULL) {
-    g_print ("\ncannot read file %s\n", score_path);
-    g_free (score_path);
-    return FALSE;
-  }
-  stat (score_path, &file_info);	/* We need check if score_path is empty */
-  for (i = 0; (i < 10) && (!feof (score_file)); i++) {
-    for (j = 0; j < 256; j++) {
-      fscanf (score_file, "%c", &name[i][j]);
-      if (name[i][j] == '\t')
-	goto a;
-      else if (name[i][j] == '\n')
-	fscanf (score_file, "%c", &name[i][j]);
-    }
+gboolean read_score (void)
+{
+   gint i, j;
+   gint cksum;
+   struct stat file_info;
+   char * score_path = choose_score_file ();
+   FILE * score_file;
+
+   if ((score_file = fopen (score_path, "r")) == NULL) {
+      g_print ("\ncannot read file %s\n", score_path);
+      g_free (score_path);
+      return FALSE;
+   }
+   stat (score_path, &file_info);	/* We need check if score_path is empty */
+   for (i = 0; (i < 10) && (!feof (score_file)); i++)
+   {
+      for (j = 0; j < 256; j++) {
+          fscanf (score_file, "%c", &name[i][j]);
+          if (name[i][j] == '\t')
+             goto a;
+          else if (name[i][j] == '\n')
+             fscanf (score_file, "%c", &name[i][j]);
+      }
   a:
-    name[i][j] = '\0';
-    fscanf (score_file, "%d", &saved_score[i]);
-    fscanf (score_file, "%x", &cksum);
-    if (check_line (i) != cksum) {
-      if (file_info.st_size)
-	g_print ("\n line No. %d in file %s is corrupted\n", i + 1, score_path);
-      strcpy (name[i], "<none>");
-      saved_score[i] = 0;
-    }
-  }
-  if (i < 9)
-    for (j = i; j < 10; j++) {
-      name[j][0] = '\0';
-      saved_score[j] = 0;
-    }
+      name[i][j] = '\0';
+      fscanf (score_file, "%d", &saved_score[i]);
+      fscanf (score_file, "%x", &cksum);
+      if (check_line (i) != cksum) {
+         if (file_info.st_size) {
+            g_print ("\n line No. %d in file %s is corrupted\n", i + 1, score_path);
+         }
+         strcpy (name[i], "<none>");
+         saved_score[i] = 0;
+      }
+   }
 
-  g_free (score_path);
-  fclose (score_file);
-  return TRUE;
+   if (i < 9) {
+      for (j = i; j < 10; j++) {
+         name[j][0] = '\0';
+         saved_score[j] = 0;
+      }
+   }
+
+   g_free (score_path);
+   fclose (score_file);
+   return TRUE;
 }
 
-gboolean
-write_score (void)
+
+gboolean write_score (void)
 {
-  gint i;
-  char * score_path = choose_score_file ();
-  FILE * score_file;
+   gint i;
+   char * score_path = choose_score_file ();
+   FILE * score_file;
 
-  score_file = fopen (score_path, "w");
-  if (!score_file) {
-    g_print ("\ncannot write file %s", score_path);
-    g_free (score_path);
-    return FALSE;
-  }
-  for (i = 0; i < 10; i++) {
-    if (strlen (name[i]) == 0)
-      strcpy (name[i], "<none>");
-    fprintf (score_file, "%s\t%d\t%x\n", name[i], saved_score[i],
-	     check_line (i));
-  }
+   score_file = fopen (score_path, "w");
+   if (!score_file) {
+      g_print ("\ncannot write file %s", score_path);
+      g_free (score_path);
+      return FALSE;
+   }
 
-  g_free (score_path);
-  fclose (score_file);
-  return TRUE;
+   for (i = 0; i < 10; i++) {
+       if (strlen (name[i]) == 0) {
+          strcpy (name[i], "<none>");
+       }
+       fprintf (score_file, "%s\t%d\t%x\n", name[i], saved_score[i],
+       check_line (i));
+   }
+
+   g_free (score_path);
+   fclose (score_file);
+   return TRUE;
 }
 
-gboolean
-insert_new_score (void)
-{
-  gint i = 0, j;
 
-  if (score <= 0)
-    return FALSE;
-  while (saved_score[i] >= score)
-    i++;
-  if (i > 9)
-    return FALSE;
-  for (j = 8; j >= i; j--) {
-    strcpy (name[j + 1], name[j]);
-    saved_score[j + 1] = saved_score[j];
-  }
-  name_i = i;
-  create_name_dialog ();
-  return TRUE;
+gboolean insert_new_score (void)
+{
+   gint i = 0, j;
+
+   if (score <= 0) {
+      return FALSE;
+   }
+   while (saved_score[i] >= score) {
+      i++;
+   }
+   if (i > 9) {
+      return FALSE;
+   }
+   for (j = 8; j >= i; j--) {
+      strcpy (name[j + 1], name[j]);
+      saved_score[j + 1] = saved_score[j];
+   }
+   name_i = i;
+   create_name_dialog ();
+   return TRUE;
 }
 
-gint
-check_line (gint i)
+
+gint check_line (gint i)
 {
-  gint j, ckline = 0;
-  div_t result;
-  for (j = 0; j < 256; j++) {
-    ckline = ckline + (gint) name[i][j];
-    if (!name[i][j])
-      break;
-  }
-  ckline = ckline + saved_score[i] * 3;
-  result = div ((int) ckline, 1111);
-  return (gint) result.rem;
+   gint j, ckline = 0;
+   div_t result;
+   for (j = 0; j < 256; j++) {
+      ckline = ckline + (gint) name[i][j];
+      if (!name[i][j]) {
+         break;
+      }
+   }
+   ckline = ckline + saved_score[i] * 3;
+   result = div ((int) ckline, 1111);
+   return (gint) result.rem;
 }
 
